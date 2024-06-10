935d11c6ac957ada29b52d38f3e80fe265905c48
alumnos alumnos <alumnos@new.edu.ar>
2024-06-10 15:30:00 -0300

    mi primer commit

---
+++ minion.py
@@ -0,0 +1,12 @@
+valid_options = 0
+
+while want_greet == "s" :
+    print('Hola que tal!')
+          want_greet = input('¿Quiere otro saludo?')
+          if want_greet not in "SN"
+          print('No le he entendido pero lo saludo igaul')
+          want_greet = "S"
+          continue
+    valid_options += 1
+    print(f'{valid_options} respuestas validas')
+    print('Que tenga un buen dia')
